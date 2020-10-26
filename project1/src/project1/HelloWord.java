a57fab11466b7595a88916f78839d204268eed89
19jy0229 <19jy0229@PC20.jynet2.jec.ac.jp>
2020-10-26 14:28:41 +0900

    add Javaと表示する行を追加

--- project1/src/project1/HelloWord.java
+++ project1/src/project1/HelloWord.java
@@ -3,8 +3,8 @@ package project1;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("Java");
 	}
 
 }
